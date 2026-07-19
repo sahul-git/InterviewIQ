b9adb8c904c21d7ad7949958c34e9c342c496644
Sahul Kumar <149247512+sahul-git@users.noreply.github.com>
2026-07-19 17:23:48 +0530

    Change API base URL to production server

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://interviewiq-backend-oou9.onrender.com',
     withCredentials: true
 });
 
