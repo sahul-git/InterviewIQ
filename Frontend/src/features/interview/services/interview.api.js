5d01e0ed64828c46f21a1de18c455b5860877951
Sahul Kumar <149247512+sahul-git@users.noreply.github.com>
2026-08-12 01:51:34 +0530

    Update interview.api.js

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://interviewiq-frontend-yh10.onrender.com",
+    baseURL: "https://interviewiq-backend-oou9.onrender.com",
     withCredentials: true,
 })
 
